2f42e5e7b59e7ba63cb029c3f9210f8665cdaf6e
nsym.kuk <k025g1028@g.neec.ac.jp>
2026-05-13 15:30:35 +0900

    masterに01_01の内容をmainに追加

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,19 +1,38 @@
-#include <stdio.h>
-#include <windows.h>
+#include<iostream>
+#include<Windows.h>
+#include<stdio.h>
 
-//master
+//01_01
 
+//関数テンプレート
+template <typename Type>
+Type Min(Type a, Type b) {
+    return (a < b) ? a : b;
+}
 
-int main()
-{
+template <>
+char Min<char>(char a, char b) {
 
-	
-	SetConsoleOutputCP(65001);
+    printf("数字以外代入できない\n");
+    return '\0';
+}
 
-	char str[] = "こんにちは世界";
+int main() {
 
-	printf("%s\n", str);
+    int i1 = 10;
+    int i2 = 20;
+    float f1 = 5.5f;
+    float f2 = 2.2f;
+    double d1 = 3.14;
+    double d2 = 3.14159;
+    char c1 = 'A';
+    char c2 = 'B';
 
-	return 0;
+    SetConsoleOutputCP(65001);
+    printf("%d\n", Min<int>(i1, i2));
+    printf("%f\n", Min<float>(f1, f2));
+    printf("%f\n", Min<double>(d1, d2));
+    printf("%c\n", Min<char>(c1, c2));
 
+    return 0;
 }
