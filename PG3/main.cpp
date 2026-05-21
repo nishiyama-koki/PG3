38a259ef45dfac16009f5489321bee4cc08fb56a
nsym.kuk <k025g1028@g.neec.ac.jp>
2026-05-21 15:10:06 +0900

    確認課題01_04完了

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -3,38 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <time.h>   
+#include <functional>
 
-//01_03
+using PFunc = std::function<void(int)>;
 
-typedef void (*PFunc)(int, int);
+//01_04
 
-// コールバック関数
-void JudgeResult(int dice, int userGuess) {
-    // 出目が奇数(1)か偶数(2)かを判定
-    int result = (dice % 2 != 0) ? 1 : 2;
-
-    printf("\n出目: %d (%s)\n", dice, (result == 1) ? "半 / 奇数" : "丁 / 偶数");
-
-    if (userGuess == result) {
-        printf("正解\n");
-    }
-    else {
-        printf("不正解\n");
-    }
-}
-
-// タイムアウト処理
-void SetTimeOut(PFunc p, int second, int dice, int userGuess) {
+void SetTimeOut(PFunc p, int second, int dice) {
     printf("%d秒待機中...\n", second);
     fflush(stdout);
     Sleep(second * 1000);
-    p(dice, userGuess);
+    p(dice);
 }
 
 int main() {
     srand((unsigned int)time(NULL));
 
-    printf("丁半ゲーム！[1:半(奇数) / 2:丁(偶数)] 入力してください: ");
+    printf("丁半ゲーム[1:半(奇数) / 2:丁(偶数)] 入力してください: ");
 
     int userGuess;
     scanf_s("%d", &userGuess);
@@ -46,8 +31,20 @@ int main() {
 
     int dice = (rand() % 6) + 1;
 
-    PFunc p = JudgeResult;
-    SetTimeOut(p, 3, dice, userGuess);
+    PFunc p = [userGuess](int d) {
+        int result = (d % 2 != 0) ? 1 : 2;
+
+        printf("\n出目: %d (%s)\n", d, (result == 1) ? "半 / 奇数" : "丁 / 偶数");
+
+        if (userGuess == result) {
+            printf("正解\n");
+        }
+        else {
+            printf("不正解\n");
+        }
+        };
+
+    SetTimeOut(p, 3, dice);
 
     return 0;
 }
