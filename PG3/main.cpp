6079d92c6401fcbcc2e95acb0634c4f153e15b1e
nsym.kuk <k025g1028@g.neec.ac.jp>
2026-07-02 16:36:23 +0900

    masterに02_02の内容を追加

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -9,64 +9,43 @@
 
 using namespace std;
 
-class Enemy {
+template <typename Type1, typename Type2>
+class ClassTemplate
+{
 public:
-    void Update();
 
-    void Attack();
-    void Escape();
-    void Approach();
-    static void (Enemy::* table[])();
-    int index = 0;
-};
+	Type1 num1;
+	Type2 num2;
 
-void Enemy::Attack() {
-    printf("敵は攻撃してきた！\n");
-}
+	ClassTemplate(Type1 n1, Type2 n2) : num1(n1), num2(n2) {}
 
-void Enemy::Escape() {
-    printf("敵は逃げ出した！\n");
-}
+	Type1 min() {
+		if (num1 < num2) {
+			return num1;
+		}
+		else {
+			return num2;
+		}
+	}
 
-void Enemy::Approach() {
-    printf("敵は近づいてきた！\n");
-}
-
-void Enemy::Update() {
-    // 関数テーブルから関数を実行
-    (this->*table[index])();
-
-    cout << ": ";
-    int input;
-    cin >> input;
-
-    if (cin.fail()) {
-        cin.clear(); 
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
-        input = -1; 
-    }
-
-    if (input == 0) {
-        index = (index + 1) % 3;
-    }
-}
-
-// メンバ関数ポインタテーブル
-void (Enemy::* Enemy::table[])() = {
-    &Enemy::Approach,
-    &Enemy::Attack,
-    &Enemy::Escape,
 };
 
 int main()
 {
+	ClassTemplate<int, int> intInt(10, 20);
+	ClassTemplate<int, float> intFloat(10, 5.5f);
+	ClassTemplate<int, double> intDouble(10, 3.14);
+	ClassTemplate<float, float> floatFloat(10.5f, 20.5f);
+	ClassTemplate<float, double> floatDouble(10.5f, 4.25);
+	ClassTemplate<double, double> doubleDouble(10.0, 20.5);
+	
+	cout << intInt.min() << endl;
+	cout << intFloat.min() << endl;
+	cout << intDouble.min() << endl;
+	cout << floatFloat.min() << endl;
+	cout << floatDouble.min() << endl;
+	cout << doubleDouble.min() << endl;
 
-    Enemy enemy;
 
-    while (1)
-    {
-        enemy.Update();
-    }
-
-    return 0;
+	return 0;
 }
